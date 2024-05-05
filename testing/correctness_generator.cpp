a22bce42f0f2c525d8ff0bae97de3105c49bfd73
sumitkk10 <sumitkk01010@gmail.com>
2024-05-06 01:21:02 +0530

    fixes

--- testing/correctness_generator.cpp
+++ testing/correctness_generator.cpp
@@ -11,75 +11,6 @@ int genNum(int l, int r) {
     return l + rand() % (r - l + 1);
 }
 
-vector<int> genArr(int n, int l, int r) {
-    vector<int> res;
-    for (int i = 0; i < n; i++) {
-        res.push_back(genNum(l, r));
-    }
-    return res;
-}
-
-vector<int> genArrNoRep(int n, int l, int r) {
-    vector<int> res;
-    map<int, int> done;
-    for (int i = 0; i < n; i++) {
-        int cur = genNum(l, r);
-        while (done[cur]) {
-            cur = genNum(l, r);
-        }
-        done[cur] = 1;
-        res.push_back(cur);
-    }
-    return res;
-}
-
-string genStrAlpha(int n, char ccase) {
-    string res = "";
-    for (int i = 0; i < n; i++) {
-        res.push_back(ccase + rand() % 26);
-    }
-    return res;
-}
-
-string genStrBinary(int n, char one, char two) {
-    string res = "";
-    for (int i = 0; i < n; i++) {
-        if (rand() % 2 == 0)
-            res.push_back(one);
-        else
-            res.push_back(two);
-    }
-    return res;
-}
-
-string genStrTernary(int n, char one, char two, char three) {
-    string res = "";
-    for (int i = 0; i < n; i++) {
-        if (rand() % 3 == 0)
-            res.push_back(one);
-        else if (rand() % 3 == 1)
-            res.push_back(two);
-        else
-            res.push_back(three);
-    }
-    return res;
-}
-
-string genStrQuad(int n, char one, char two, char three, char four) {
-    string res = "";
-    for (int i = 0; i < n; i++) {
-        if (rand() % 4 == 0)
-            res.push_back(one);
-        else if (rand() % 4 == 1)
-            res.push_back(two);
-        else if (rand() % 4 == 2)
-            res.push_back(three);
-        else
-            res.push_back(four);
-    }
-    return res;
-}
-
 void genGraph(int n, int m) {
     m = min(m, (n * (n - 1)) / 2);
     cout << n << ' ' << m << endl;
@@ -103,38 +34,6 @@ void genGraph(int n, int m) {
     return ;
 }
 
-vector<int> genPerm(int n) {
-    vector<int> res;
-    for (int i = 0; i < n; i++)
-        res.push_back(i + 1);
-    random_shuffle(res.begin(), res.end());
-    return res;
-}
-
-void genTree(int n) {
-    assert(n >= 0);
-    int permutation[n + 14];
-    vector<pair<int, int>> res(n ? n - 1 : 0);
-    iota(permutation, permutation + 1 + n, 0);
-    shuffle(permutation + 1, permutation + 1 + n, rng);
-    for (int i = 2; i <= n; ++i) {
-        int u = i, v = genNum(1, i - 1);
-        u = permutation[u], v = permutation[v];
-        res[i - 2] = minmax(u, v);
-    }
-    shuffle(res.begin(), res.end(), rng);
-    cout << n << endl;
-    for (auto x : res)
-        cout << x.first << ' ' << x.second << endl;
-    return ;
-}
-
-string genKAlphaString(int n, int k) {
-    string res = "";
-    for (int i = 0; i < n; i++)
-        res.push_back('a' + rand() % k);
-    return res;
-}
 
 int32_t main() {
     srand(chrono::steady_clock::now().time_since_epoch().count());
